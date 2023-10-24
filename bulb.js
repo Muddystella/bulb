82bc5e6129bbb4c906305eba0718e62f9cb940a4
Muddystella <133777833+Muddystella@users.noreply.github.com>
2023-10-24 14:42:29 +0100

    initial comit

---
+++ bulb.js
@@ -0,0 +1,9 @@
+function bulboff() {
+    let myBulb = "off-bulb.png";
+    document.getElementById("Image").src = myBulb;
+}
+
+function bulbon() {
+    let myBulb = "light-bulb.png";
+    document.getElementById("Image").src = myBulb;
+}
